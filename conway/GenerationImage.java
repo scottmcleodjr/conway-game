09380a5c419e1d69aebc7a6bfec919334a24a5fa
S M McLeod <s.mcleodjr@gmail.com>
2020-02-19 00:42:45 +0000

    Revert "Remove unused code for larger cell sizes"
    This reverts commit f86c080a45cbbdbd149c6d271901528955e18f23

--- conway/GenerationImage.java
+++ conway/GenerationImage.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 
 public class GenerationImage extends BufferedImage {
 
-  public GenerationImage(byte[][] state, int xPixels, int yPixels, Color liveColor) {
+  public GenerationImage(byte[][] state, int xPixels, int yPixels, int pixelsPerCell, Color liveColor) {
     // Create a buffered Image and then make it match array
     super(xPixels, yPixels, BufferedImage.TYPE_INT_RGB);
 
@@ -15,6 +15,15 @@ public class GenerationImage extends BufferedImage {
         if (state[r][c] == 1) {
           // R and C (-1) because the above starts at 1,1 and the image starts at 0,0
           setRGB(c - 1, r - 1, liveColor.getRGB());
+
+          // The below is used for cells>1x1px.
+          /*
+           * imageR and imageC coorespond to the upper left pixel of the cell in the
+           * image. The image's .setRGB() method starts at (x,y) as (0,0). int imageR =
+           * (r-1) * pixelsPerCell; int imageC = (c-1) * pixelsPerCell; for (int row =
+           * imageR; row < (imageR + pixelsPerCell); row++) { for (int col = imageC; col <
+           * (imageC + pixelsPerCell); col++) { setRGB(col, row, liveColor.getRGB()); } }
+           */
         }
       }
     }
