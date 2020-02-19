00c3d8bd531e6c418c6e3b473aa0c57c3e6f23f2
scottmcleod <s.mcleodjr@gmail.com>
2020-02-18 20:00:13 -0500

    Remove unused code for larger cell sizes

--- conway/Controller.java
+++ conway/Controller.java
@@ -10,9 +10,6 @@ public class Controller {
 
   private int cellsWide;
   private int cellsHigh;
-  private int pixelsPerCell;
-  private int imageWidth;
-  private int imageHeight;
   private Color liveColor;
   private Timer timer;
   private int timerDelayMillis; // Milliseconds
@@ -21,21 +18,20 @@ public class Controller {
   private GenerationImage image;
   private GenerationImageFrame frame;
 
-  public Controller(int pixels, int delay, StartSeedStyle style, boolean launchFullScreen, Color liveColor) {
+  public Controller(int delay, StartSeedStyle style, boolean launchFullScreen, Color liveColor) {
     cellsWide = Toolkit.getDefaultToolkit().getScreenSize().width;
     cellsHigh = Toolkit.getDefaultToolkit().getScreenSize().height;
     if (!launchFullScreen) {
-      cellsWide *= 0.8; // Fill most of the screen
+      // Fill most of the screen if not full screen
+      cellsWide *= 0.8;
       cellsHigh *= 0.8;
     }
-    pixelsPerCell = pixels;
-    imageWidth = cellsWide * pixelsPerCell;
-    imageHeight = cellsHigh * pixelsPerCell;
+
     timerDelayMillis = delay;
     this.liveColor = liveColor;
 
     state = new Generation((cellsHigh + 2), (cellsWide + 2), style); // +2 to add border values
-    image = new GenerationImage(state.getCurrentGeneration(), imageWidth, imageHeight, pixelsPerCell, liveColor);
+    image = new GenerationImage(state.getCurrentGeneration(), cellsWide, cellsHigh, liveColor);
     frame = new GenerationImageFrame(image, launchFullScreen);
 
     timer = new Timer(timerDelayMillis, new TimerEvent());
@@ -48,7 +44,7 @@ public class Controller {
   private class TimerEvent implements ActionListener {
     public void actionPerformed(ActionEvent e) {
       state.advangeGeneration();
-      image = new GenerationImage(state.getCurrentGeneration(), imageWidth, imageHeight, pixelsPerCell, liveColor);
+      image = new GenerationImage(state.getCurrentGeneration(), cellsWide, cellsHigh, liveColor);
       frame.updateFrameImage(image);
     }
   }
