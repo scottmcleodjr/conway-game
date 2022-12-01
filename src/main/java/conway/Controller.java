582a076b5a7fa609fa7cd92d531c8ce28cd961c2
S M McLeod <s.mcleodjr@gmail.com>
2022-12-01 08:05:34 -0500

    use composition for GenerationImageFrame class

--- src/main/java/conway/Controller.java
+++ src/main/java/conway/Controller.java
@@ -22,6 +22,7 @@ public class Controller {
   }
 
   public void start() {
+    frame.show();
     timer.start();
   }
 
--- src/main/java/conway/GenerationImageFrame.java
+++ src/main/java/conway/GenerationImageFrame.java
@@ -10,37 +10,41 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class GenerationImageFrame extends JFrame {
+public class GenerationImageFrame {
 
+  private JFrame frame;
   private JLabel imageLabel;
 
   public GenerationImageFrame(BufferedImage image, boolean launchFullScreen) {
-    setTitle("Conway's Game");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    getContentPane().setBackground(Color.BLACK);
+    frame = new JFrame();
+    frame.setTitle("Conway's Game");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setBackground(Color.BLACK);
 
     if (launchFullScreen) {
       // Remove the title bar if you are full screen
-      setUndecorated(true);
+      frame.setUndecorated(true);
     } else {
       // Get the frame off the upper edge of the screen if not
       int w = (int) (Toolkit.getDefaultToolkit().getScreenSize().width * 0.1);
       int h = (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.1);
-      setLocation(w, h);
+      frame.setLocation(w, h);
     }
 
     try {
-      setIconImage(ImageIO.read(getClass().getResource("/icon.png")));
+      frame.setIconImage(ImageIO.read(getClass().getResource("/icon.png")));
     } catch (Exception e) {
       System.out.println("[WARNING] Unable to load icon image: " + e.getMessage());
     }
 
     imageLabel = new JLabel(new ImageIcon(image));
     imageLabel.addMouseListener(new ImageClick());
-    add(imageLabel);
-    pack();
+    frame.add(imageLabel);
+    frame.pack();
+  }
 
-    setVisible(true);
+  public void show() {
+    frame.setVisible(true);
   }
 
   public void updateFrameImage(BufferedImage image) {
